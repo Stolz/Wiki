e3b5a52d145a44337b346eb6172b3650215966ee
Stolz <Stolz@users.noreply.github.com>
2015-02-28 23:47:07 +0100

    php artisan fresh

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,3 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
