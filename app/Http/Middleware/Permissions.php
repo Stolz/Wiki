ac0ea2be32348cd30cf57e07ab2b3167c521cded
Stolz <Stolz@users.noreply.github.com>
2015-05-25 12:04:22 +0200

    Allow nested resources

--- app/Http/Middleware/Permissions.php
+++ app/Http/Middleware/Permissions.php
@@ -14,15 +14,21 @@ class Permissions extends Authenticate
 		if ($this->auth->guest())
 			return parent::handle($request, $next);
 
-		list($resource, $action) = explode('.', $request->route()->getName());
+		// Last segment of the route name is the action, the rest is the resource
+		$routeName = $request->route()->getName();
+		$pivot = strrpos($routeName, '.');
+		$resource = substr($routeName, 0, $pivot);
+		$action = substr($routeName, $pivot + 1);
 
 		if ($this->auth->user()->can($action, $resource))
 			return $next($request);
 
-		if ($request->ajax())
-			return response('Unauthorized.', 401);
+		$error = sprintf(_("Your user is not authorized to perform action '%s' on resource '%s'"), $action, $resource);
 
-		\Session::flash('error', _('Your user is not authorized to access this section'));
+		if ($request->ajax())
+			return response($error, 401);
+
+		\Session::flash('error', $error);
 
 		return redirect()->route('home');
 	}
