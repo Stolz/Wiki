03e3c84db1fa5a8b41c779deb006665711e6a0dd
Stolz <Stolz@users.noreply.github.com>
2015-06-29 15:39:22 +0200

    Use marked.js from NPM

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,42 @@
-// Bower components path
-var bower = 'resources/assets/bower/';
+var foundation = {
+	// Base path for Bower components
+	path: 'resources/assets/bower/foundation/',
 
-// Zurb Foundation vendor dependencies
-var vendor = ['vendor/modernizr.js', 'vendor/jquery.js'];
+	// Vendor dependencies
+	vendor: ['vendor/modernizr.js', 'vendor/jquery.js', 'vendor/fastclick.js'],
 
-// Zurb Foundation components
-var components = [
-	'foundation/foundation.js',
-	//'foundation/foundation.abide.js',
-	//'foundation/foundation.accordion.js',
-	'foundation/foundation.alert.js',
-	//'foundation/foundation.clearing.js',
-	'foundation/foundation.dropdown.js',
-	//'foundation/foundation.equalizer.js',
-	//'foundation/foundation.interchange.js',
-	//'foundation/foundation.joyride.js',
-	//'foundation/foundation.magellan.js',
-	'foundation/foundation.offcanvas.js',
-	//'foundation/foundation.orbit.js',
-	'foundation/foundation.reveal.js',
-	//'foundation/foundation.slider.js',
-	'foundation/foundation.tab.js',
-	//'foundation/foundation.tooltip.js',
-	//'foundation/foundation.topbar.js'
-];
+	// Components
+	components: [
+		'foundation/foundation.js',
+		//'foundation/foundation.abide.js',
+		//'foundation/foundation.accordion.js',
+		'foundation/foundation.alert.js',
+		//'foundation/foundation.clearing.js',
+		'foundation/foundation.dropdown.js',
+		//'foundation/foundation.equalizer.js',
+		//'foundation/foundation.interchange.js',
+		//'foundation/foundation.joyride.js',
+		//'foundation/foundation.magellan.js',
+		'foundation/foundation.offcanvas.js',
+		//'foundation/foundation.orbit.js',
+		'foundation/foundation.reveal.js',
+		//'foundation/foundation.slider.js',
+		'foundation/foundation.tab.js',
+		//'foundation/foundation.tooltip.js',
+		//'foundation/foundation.topbar.js'
+	]
+}
 
 var elixir = require('laravel-elixir');
 elixir.config.sourcemaps = false;
 elixir(function(mix) {
 
 	// Compile CSS
-	mix.sass('app.scss', 'public/css/foundation.css', {includePaths: [bower + 'foundation/scss']});
+	mix.sass('app.scss', 'public/css/foundation.css', {includePaths: [foundation.path + 'scss']});
 
 	// Compile JavaScript
-	mix.scripts(vendor.concat(components), 'public/js/foundation.js', bower + 'foundation/js');
+	mix.scripts(foundation.vendor.concat(foundation.components), 'public/js/foundation.js', foundation.path + 'js');
+	mix.copy('node_modules/marked/marked.min.js', 'public/js/marked.js');
+
 
 });
