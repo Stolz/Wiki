ccf295c412757b929df13478607dd875bc313195
Stolz <Stolz@users.noreply.github.com>
2015-07-29 16:34:13 +0200

    Added login throttling from upstream

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,15 +3,14 @@
 use App\Http\Requests\SocialLoginRequest;
 use App\Provider;
 use App\User;
-use Config;
-use Input;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Http\Request;
 use Laravel\Socialite\Contracts\User as SocialUser;
-use Session;
-use Socialite;
-use URL;
 
 class AuthController extends Controller
 {
+	use ThrottlesLogins;
+
 	/**
 	 * Show login form
 	 *
@@ -25,15 +24,30 @@ class AuthController extends Controller
 	/**
 	 * Attempt to log in an user using a social authentication provider.
 	 *
+	 * @param  Illuminate\Http\Request $request
 	 * @param  string
 	 * @return Response
 	 */
-	public function loginWithProvider($providerSlug)
+	public function loginWithProvider(Request $request, $providerSlug)
 	{
+		// Use provider name as the throttling cache key
+		$request['provider'] = $providerSlug;
+
+		// Check if there are too many login attempts for current provider and IP
+		if($this->hasTooManyLoginAttempts($request))
+		{
+			// Flash error message
+			$seconds = (int) \Cache::get($this->getLoginLockExpirationKey($request)) - time();
+			$message = sprintf(_('Please try again in %d seconds.'), $seconds);
+
+			return $this->goBack(_('Too many login attempts') . '. ' . $message);
+		}
+		$this->incrementLoginAttempts($request);
+
 		// If the remote provider sends an error cancel the process
 		foreach(['error', 'error_message', 'error_code'] as $error)
-			if(Input::has($error))
-				return $this->goBack(_('Something went wrong') . '. ' . Input::get($error));
+			if($request->has($error))
+				return $this->goBack(_('Something went wrong') . '. ' . $request->get($error));
 
 		// Get provider
 		$provider = Provider::whereSlug($providerSlug)->firstOrFail();
@@ -43,14 +57,18 @@ class AuthController extends Controller
 			return $this->goBack(_('Unavailable provider'));
 
 		// Set provider callback url
-		Config::set("services.{$provider->slug}.redirect", URL::current());
+		\Config::set("services.{$provider->slug}.redirect", \URL::current());
 
 		// Create an Oauth service for this provider
-		$oauthService = Socialite::with($provider->slug);
+		$oauthService = \Socialite::with($provider->slug);
 
 		// Check if current request is a callback from the provider
-		if(Input::has('oauth_token')/*Oauth 1*/ or Input::has('code')/*Oauth 2*/)
+		if($request->has('oauth_token')/*Oauth 1*/ or $request->has('code')/*Oauth 2*/)
+		{
+			$this->clearLoginAttempts($request);
+
 			return $this->loginSocialUser($provider, $oauthService->user());
+		}
 
 		// If we have configured custom scopes use them
 		if($scopes = config("services.{$provider->slug}.scopes"))
@@ -113,8 +131,18 @@ class AuthController extends Controller
 	 */
 	protected function goBack($error)
 	{
-		Session::flash('error', $error);
+		\Session::flash('error', $error);
 
 		return redirect()->back();
+	}
+
+	/**
+	 * Get the name of the attribute used to control login throttling.
+	 *
+	 * @return string
+	 */
+	protected function loginUsername()
+	{
+		return 'provider';
 	}
 }
