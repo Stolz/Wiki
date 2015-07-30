abaacafae1b45e4ba3c36db6fc0c7199969a56a3
Stolz <Stolz@users.noreply.github.com>
2015-07-30 16:06:34 +0200

    Language middleware

---
+++ app/Http/Middleware/Language.php
@@ -0,0 +1,20 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+
+class Language
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		app('language'); // Workaround
+
+		return $next($request);
+	}
+}
