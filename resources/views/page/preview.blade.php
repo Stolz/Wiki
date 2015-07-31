fe36c70584c5515fd1c3ee779805d2dfba20859c
Stolz <Stolz@users.noreply.github.com>
2015-07-31 12:10:05 +0200

    Added scroll sync to page editor

--- resources/views/page/preview.blade.php
+++ resources/views/page/preview.blade.php
@@ -2,21 +2,53 @@
 
 <?php Assets::add(['wiki.css', 'marked.js']) ?>
 
-<fieldset>
-	<legend><label>{{ _('Preview') }}</label></legend>
-	<div id="preview">{!! $resource->markup !!}</div>
+<fieldset id="preview">
+	<legend>{{ _('Preview') }}</legend>
+	<h1 id="previewTitle">{!! $resource->name !!}</h1>
+	<div id="previewBody">{!! $resource->markup !!}</div>
 </fieldset>
+
+
 
 @section('js')
 @parent
 <script>
 $(document).ready(function() {
 
-	var $source = $("#source"), $preview = $("#preview");
-	$source.keyup(function () {
-		$preview.html(marked($(this).val()));
+	var $title = $("#name"), $body = $("#source"), $previewTitle = $("#previewTitle"), $previewBody = $("#previewBody");
+
+	// Live preview
+	$title.keyup(function () {
+		$previewTitle.text($title.val());
 	});
+	$body.keyup(function () {
+		$previewBody.html(marked($body.val()));
+	});
+
+	// Sync scroll
+	var $scrollers = $('#source, #preview');
+	var syncScroll = function(e){
+		var $other = $scrollers.not(this).off('scroll'), other = $other.get(0);
+		var percentage = this.scrollTop / (this.scrollHeight - this.offsetHeight);
+		other.scrollTop = percentage * (other.scrollHeight - other.offsetHeight);
+		// Firefox workaround. Rebinding without delay isn't enough.
+		setTimeout(function(){ $other.on('scroll', syncScroll ); },10);
+	}
+	$scrollers.on('scroll', syncScroll);
 
 });
 </script>
+@stop
+
+
+
+
+
+@section('css')
+<style>
+#preview{
+	max-height:25em;
+	overflow-y:scroll
+}
+</style>
 @stop
